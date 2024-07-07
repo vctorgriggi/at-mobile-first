984a958c3f7b08d5b835a11cc56ced225c5ff1c0
vctorgriggi <victor.gsilva@al.infnet.edu.br>
2024-07-07 17:20:54 -0400

    feat: implementar lazy loading ao transicionar entre páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import List from "./pages/List";
-import Form from "./pages/Form";
+import CircularIndeterminate from "./components/Loading";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const lazyWithDelay = (importFunction, delayTime = 1000) =>
+  lazy(() =>
+    Promise.all([importFunction(), delay(delayTime)]).then(
+      ([moduleExports]) => moduleExports
+    )
+  );
+
+const SignIn = lazyWithDelay(() => import("./pages/SignIn"));
+const SignUp = lazyWithDelay(() => import("./pages/SignUp"));
+const Form = lazyWithDelay(() => import("./pages/Form"));
+const Home = lazyWithDelay(() => import("./pages/Home"));
+const List = lazyWithDelay(() => import("./pages/List"));
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/form" element={<Form />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <CircularIndeterminate
+            sx={{
+              height: "100vh",
+              alignItems: "center",
+            }}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
